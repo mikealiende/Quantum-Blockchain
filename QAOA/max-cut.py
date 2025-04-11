1135cd321419d96f737060dc6b15370721e350bc
Aliende Garcia Miguel <Miguel.Aliende@bshg.com>
2025-04-11 16:08:05 +0200

    first maxcut pennylane

--- QAOA/max-cut.py
+++ QAOA/max-cut.py
@@ -1,23 +1,79 @@
-import networkx as nx
+import pennylane as qml
+from pennylane import numpy as np
+import networkx as nx 
 import matplotlib.pyplot as plt
-import numpy as np
 
-from qiskit_optimization.applications import Maxcut
+# --- 1. DEFINIR EL GRAFO ---
 
-# ---1. Definir grafo---
-num_nodes = 4
-np.random.seed(123) #para reproducibilidad del grafo
-adj_matrix = np.random.randint(0, 2, size=(num_nodes, num_nodes))
-adj_matrix = np.triu(adj_matrix, 1) # Hacer la matriz triangular superior
+num_nodes = 6
+np.random.seed(123)
+adj_matrix = np.random.randint(0,2, size=(num_nodes, num_nodes))
+adj_matrix = np.triu(adj_matrix, 1)
 graph = nx.from_numpy_array(adj_matrix)
 
-#Dibujar el grafo
+#  Dibujar grafo
 pos = nx.spring_layout(graph, seed=7)
 nx.draw(graph, pos, with_labels=True, node_color='lightblue', edge_color='gray')
-plt.title("Grafo de entrada")
+plt.title("Grafo de Ejemplo para Max-Cut (PennyLane)")
 plt.show()
 
-# ---2. Definir el problema de Max-Cut---
-maxcut = Maxcut(graph)
-qp = maxcut.to_quadratic_program()
-print(f"Cuadratic Program: {qp.prettyprint()}")
+# Hamiltoniano de Costo (H_C): Suma de Z_i Z_j sobre las aristas (i,j)
+cost_h, mixer_h = qml.qaoa.maxcut(graph=graph)
+
+print("Hamiltoniano de Costo H_C:")
+print(cost_h)
+
+# --- Simulamos dispositivo cuantico ---
+dev = qml.device('defuault.qubit', wires=num_nodes)
+
+# --- Definir circuito QAOA 
+n_layers = 2
+
+def qaoa_layer(gamma, beta):
+    qml.qaoa.cost_layer(gamma, cost_h)
+    qml.qaoa.mixer_layer(beta,mixer_h)
+    
+def circuit(param, wires):
+    gammas = param[0]
+    betas = param[1]
+    
+    #Iniciar superposición
+    for i in wires:
+        qml.Hadamard(wires=i)
+        
+    #Aplicar capas de QAOA
+    for i in range(n_layers):
+        qaoa_layer(gammas[i], betas[i])
+        
+@qml.qnode(dev)
+def cost_function(params):
+    circuit(params, wires=range(num_nodes))
+    return qml.expval(cost_h)
+
+# Optimizador clásico
+
+optimizer = qml.AdamOptimizer(stepsize=0.1)
+
+np.random.seed(42)
+init_params = np.random.uniform(0, np.pi, (2, n_layers), requires_grad=True)
+params = init_params
+
+# --- Bucle de optimizacion ---
+steps = 100
+print("\nIniciando optimizacion")
+for i in range(steps):
+    params = optimizer.step(cost_function, params)
+    if(i+1)%10 ==0:
+        cost_val = cost_function(params)
+        print(f"Paso {i+1:3d} - Costo (Energia Esperada HC:) {cost_val:.4f}")
+        
+print("optimizacion finalizada")
+optimal_params = params
+print(f"\nParametros optimos: \nGammas:{optimal_params[0]}\nBetas: {optimal_params[1]}")
+
+
+        
+    
+
+
+
